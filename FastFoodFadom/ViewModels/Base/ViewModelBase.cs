960bf82b6d40945859ca583883bf6ee4484ee0cb
numb4gt <psharilov@mail.ru>
2022-05-09 13:31:41 +0300

    Добавление базового класса модели-представления

---
+++ FastFoodFadom/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FastFoodFadom.ViewModels.Base
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] String PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
