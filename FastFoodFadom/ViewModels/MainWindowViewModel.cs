e2c630c4b081f1b27b4f8717c99fa875e235fe82
numb4gt <psharilov@mail.ru>
2022-05-09 14:40:24 +0300

    Легкие изменения

---
+++ FastFoodFadom/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FastFoodFadom.ViewModels.Base;
+
+namespace FastFoodFadom.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        private string _Title = "FastFoodFandom";
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+
+
+
+    }
+}
