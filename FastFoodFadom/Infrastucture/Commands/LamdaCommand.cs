eabc8c6806ea295fac41b9570db877f11772c178
numb4gt <psharilov@mail.ru>
2022-05-11 12:47:28 +0300

    Добавление главного класса LamdaCommand

---
+++ FastFoodFadom/Infrastucture/Commands/LamdaCommand.cs
@@ -0,0 +1,27 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Text;
+using FastFoodFadom.Infrastucture.Commands.Base;
+
+
+namespace FastFoodFadom.Infrastucture.Commands
+{
+    class LamdaCommand : Command
+    {
+
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public LamdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+
+
+        }
+
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _Execute(parameter);
+    }
+}
